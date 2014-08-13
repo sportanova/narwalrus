1424ba524d200a57fd505cf8fa2cf7d394ff1d5b
sportanova <sportano@gmail.com>
2014-08-13 03:20:08 -0700

    added tablecell for topics fetching emails - WIP

---
+++ NARTopicCell.h
@@ -0,0 +1,13 @@
+//
+//  NARTopicCell.h
+//  narwalrus
+//
+//  Created by Stephen Portanova on 8/12/14.
+//  Copyright (c) 2014 sportanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NARTopicCell : UITableViewCell
+
+@end
